3d7fbc438fc6d795093cc294d5fa64d225e6c7e4
Aphelios28 <truongphamnhat2004@gmail.com>
2026-02-09 09:53:09 +0700

    add native model

--- app/agents/base_agent.py
+++ app/agents/base_agent.py
@@ -1,4 +1,5 @@
 import os
+import google.generativeai as genai
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -12,3 +13,7 @@ class Agent:
             api_key=api_key,
             temperature=0.7
         )
+
+        genai.configure(api_key=api_key)
+        self.native_model = genai.GenerativeModel(model)
+
