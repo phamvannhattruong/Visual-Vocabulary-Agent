4b30ab4b44065aaaab78cd76b67fac8aacd2703e
Aphelios28 <truongphamnhat2004@gmail.com>
2026-08-19 09:24:44 +0700

    add schemas for chat respone and request

--- backend/app/schemas/schesmas.py
+++ backend/app/schemas/schesmas.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 class PronunciationEvaluation(BaseModel):
     score: int = Field(..., description="Điểm số phát âm từ 0 đến 100")
     feedback: str = Field(..., description="Nhận xét chi tiết bằng tiếng Việt (ví dụ: Thiếu âm cuối 's', Phát âm sai nguyên âm 'a')")
     tip: str = Field(..., description="Mẹo cải thiện phát âm bằng tiếng Việt")
+
+class ChatRequest(BaseModel):
+    messages: List[Dict[str, str]]
+
+class ChatResponse(BaseModel):
+    reply: str
